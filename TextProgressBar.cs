3f4f746dee5bca558c42634c0a3acb534cc36a38
ukushu <ukushu@users.noreply.github.com>
2018-12-19 21:30:04 +0200

    Code structure fixes

--- TextProgressBar.cs
+++ TextProgressBar.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace ProgressBarSample
@@ -18,21 +17,6 @@ namespace ProgressBarSample
 
     public class TextProgressBar : ProgressBar
     {
-        private string _text = string.Empty;
-
-        [Description("If it's empty, % will be shown"), Category("Additional Options"), Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
-        public override string Text
-        {
-            get {
-                return _text;
-            }
-            set
-            {
-                _text = value;
-                Invalidate();//redraw component after change value from VS Properties section
-            }
-        }
-
         [Description("Font of the text on ProgressBar"), Category("Additional Options")]
         public Font TextFont { get; set; } = new Font(FontFamily.GenericSerif, 11, FontStyle.Bold|FontStyle.Italic);
         
@@ -75,41 +59,27 @@ namespace ProgressBarSample
                 _visualMode = value;
                 Invalidate();//redraw component after change value from VS Properties section
             }
-        } 
-
-        private string _percentageStr { get { return $"{(int)((float)Value - Minimum) / ((float)Maximum - Minimum) * 100 } %"; } }
-        private string _currProgressStr { get { return $"{Value}/{Maximum}"; } }
-
-        private Color GetBrushColor(Brush brush)
-        {
-            var pen = new Pen(brush);
-            var penColor = pen.Color;
-            pen.Dispose();
-
-            return penColor;
         }
 
-        public TextProgressBar()
+        private string _text = string.Empty;
+
+        [Description("If it's empty, % will be shown"), Category("Additional Options"), Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
+        public override string Text
         {
-            //remove blinking/flickering
-            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
-        }
-        
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            Rectangle rect = ClientRectangle;
-            Graphics g = e.Graphics;
-            
-            ProgressBarRenderer.DrawHorizontalBar(g, rect);
-            rect.Inflate(-3, -3);
-            if (Value > 0)
+            get
             {
-                Rectangle clip = new Rectangle(rect.X, rect.Y, (int)Math.Round(((float)Value / Maximum) * rect.Width), rect.Height);
-
-                e.Graphics.FillRectangle(_progressColourBrush, clip);
+                return _text;
             }
+            set
+            {
+                _text = value;
+                Invalidate();//redraw component after change value from VS Properties section
+            }
+        }
 
-            if ( VisualMode != ProgressBarDisplayMode.NoText )
+        private string _textToDraw
+        {
+            get
             {
                 string text = string.Empty;
 
@@ -131,14 +101,68 @@ namespace ProgressBarSample
                         text = $"{Text}: {_percentageStr}";
                         break;
                 }
-                
-                SizeF len = g.MeasureString(text, TextFont);
-                Point location = new Point(((Width / 2) - (int)len.Width / 2), ((Height / 2) - (int)len.Height / 2) );
-                
-                g.DrawString(text, TextFont, _textColourBrush, location);
+
+                return text;
+            }
+        }
+        
+        private string _percentageStr { get { return $"{(int)((float)Value - Minimum) / ((float)Maximum - Minimum) * 100 } %"; } }
+
+        private string _currProgressStr { get { return $"{Value}/{Maximum}"; } }
+
+        private Color GetBrushColor(Brush brush)
+        {
+            var pen = new Pen(brush);
+            var penColor = pen.Color;
+            pen.Dispose();
+
+            return penColor;
+        }
+
+        public TextProgressBar()
+        {
+            //remove blinking/flickering
+            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            DrawProgressBar(g);
+
+            DrawStringIfNeeded(g);
+        }
+
+        private void DrawProgressBar(Graphics g)
+        {
+            Rectangle rect = ClientRectangle;
+
+            ProgressBarRenderer.DrawHorizontalBar(g, rect);
+
+            rect.Inflate(-3, -3);
+
+            if (Value > 0)
+            {
+                Rectangle clip = new Rectangle(rect.X, rect.Y, (int)Math.Round(((float)Value / Maximum) * rect.Width), rect.Height);
+
+                g.FillRectangle(_progressColourBrush, clip);
             }
         }
 
+        private void DrawStringIfNeeded(Graphics g)
+        {
+            if (VisualMode != ProgressBarDisplayMode.NoText)
+            {
+                string text = _textToDraw;
+
+                SizeF len = g.MeasureString(text, TextFont);
+                Point location = new Point(((Width / 2) - (int)len.Width / 2), ((Height / 2) - (int)len.Height / 2));
+
+                g.DrawString(text, TextFont, _textColourBrush, location);
+            }
+        }
+        
         public new void Dispose()
         {
             _textColourBrush.Dispose();
